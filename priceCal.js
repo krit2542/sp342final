6b9ebbd15b2e039fdd12730e2cb1256d0b6d62a8
Supakrit Chatreekul <2010511101024@live4.utcc.ac.th>
2022-04-27 15:01:19 +0700

    adding net price calculate fn

--- priceCal.js
+++ priceCal.js
@@ -3,7 +3,23 @@ function priceCalculate() {
   let selectObj = document.getElementById("event");
   let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
   
-  return 0;
+  if(eventId == 1){
+	 tickprice = 91.25;
+  }else if (eventId == 2){
+	 tickprice = 79.88;
+  }else if (eventId == 3){
+	 tickprice = 83.12
+  }else if (eventId == 4){
+  tickprice = 100}
+			  
+		  let net = ticknum * tickprice
+  
+  
+  
+  
+  
+  
+  return net;
 }
 
 
